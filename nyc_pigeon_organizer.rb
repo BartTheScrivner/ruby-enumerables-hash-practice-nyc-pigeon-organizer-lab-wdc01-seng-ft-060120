a1abbe13a34ff9d35a174779350aae957e5ba73f
Dave Frame <dframe22@gmail.com>
2020-04-23 00:31:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,42 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
-  pigeon_hash = {}
-  
+  #Create a list for the pigeon names
+  name_list = []
+  trait_list = []
+  #iterate over the entire array, extracting names and adding them to the above list.
+  data.each_pair do |(trait, category_hash)|
+    category_hash.each_pair do |(quality, names)|
+      names.count do |name|
+        #prevent dulicates of names mentioned more than once
+        name_list.push(name) unless name_list.include?(name)
+      end
+      #return the list
+      name_list
+    end
+    name_list
+  end
+  #convert the name list to a hash
+  name_hash = Hash[name_list.collect { |name| [name, {}] } ]
+  #iterate over the raw hash again to extract traits
+  data.each_pair {|(trait, category_hash)| trait_list.push(trait) }
+  #convert the trait list to a hash
+  trait_hash = Hash[trait_list.collect { |trait| [ trait, [] ] } ]
+  #combine name and trait hashes
+  combined_hash = name_hash.each_pair { |(name, traits)| name_hash[name] = trait_hash }
+  combined_hash.each_pair do |(key, value)|
+    data.each_pair do |(trait, category_hash)|
+      category_hash.each_pair do |(quality, names)|
+        if names.include?(key)
+          value[trait].push(quality.to_s) unless value[trait].include?(quality.to_s)
+        end
+        combined_hash
+      end
+      combined_hash
+    end
+    combined_hash
+  end
+  combined_hash
 end
 
 pigeon_data = {
